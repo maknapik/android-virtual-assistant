0209c69abf413c19d72784128bbcf434f165aeeb
MateuszKnapik <m.knapik6@gmail.com>
2020-11-09 18:05:51 +0100

    Initial commit

---
+++ app/src/main/java/pl/edu/agh/virtualassistant/MainActivity.java
@@ -0,0 +1,15 @@
+package pl.edu.agh.virtualassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}

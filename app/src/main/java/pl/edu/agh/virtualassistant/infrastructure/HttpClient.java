11d526e352285a40bd7706fde37247277b4aaa4f
MateuszKnapik <m.knapik6@gmail.com>
2020-11-12 22:55:50 +0100

    [Tech] Added HTTP client and basic network configuration

---
+++ app/src/main/java/pl/edu/agh/virtualassistant/infrastructure/HttpClient.java
@@ -0,0 +1,23 @@
+package pl.edu.agh.virtualassistant.infrastructure;
+
+import android.content.Context;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.util.function.Consumer;
+
+public class HttpClient {
+
+    public static void sendRequest(Context context, int method, String url, JSONObject jsonRequest,
+                            Consumer<JSONObject> onResponseCallback,
+                                   Consumer<VolleyError> onErrorResponseCallback) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(method, url,
+                jsonRequest, onResponseCallback::accept, onErrorResponseCallback::accept);
+
+        VolleyClient.getInstance(context).addToRequestQueue(jsonObjectRequest);
+    }
+
+}
---
+++ app/src/main/java/pl/edu/agh/virtualassistant/infrastructure/VolleyClient.java
@@ -0,0 +1,42 @@
+package pl.edu.agh.virtualassistant.infrastructure;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class VolleyClient {
+
+    @SuppressLint("StaticFieldLeak")
+    private static VolleyClient instance;
+
+    private final Context context;
+
+    private RequestQueue requestQueue;
+
+    private VolleyClient(Context context) {
+        this.context = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized VolleyClient getInstance(Context context) {
+        if (instance == null) {
+            instance = new VolleyClient(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+}
